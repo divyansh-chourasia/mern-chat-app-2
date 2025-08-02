a842f54a5ad70e82383b4e25125a3917df713f9e
Divyansh Chourasia <Divyansh Chourasia@LAPTOP-ACKKCLSS.local>
2025-08-02 17:17:43 +0530

    user route

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,15 @@ import cookieParser from "cookie-parser";
 const PORT = process.env.PORT || 5000;
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import userRoutes from "./routes/user.route.js";
 
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(cookieParser ()) // Middleware to parse cookies
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes)
+ 
  
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
