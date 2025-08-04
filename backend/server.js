de4e83d9cfc03aab14227a6c26cffa196a505476
Divyansh Chourasia <Divyansh Chourasia@LAPTOP-ACKKCLSS.local>
2025-08-04 13:23:27 +0530

    merging to main

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,16 @@ dotenv.config();
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import cookieParser from "cookie-parser";
 import { app, server } from "./socket/socket.js";
-
+import path from "path";
 
 const PORT = process.env.PORT || 5000;
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import userRoutes from "./routes/user.route.js";
+
+
+const __dirname = path.resolve();
+
 
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(cookieParser ()) // Middleware to parse cookies
@@ -21,6 +25,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes)
  
+
+
+app.use(express.static(path.join(__dirname, "/frontend/dist", )));
+
+app.get("{/*}", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
  
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
